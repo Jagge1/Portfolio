ba07126487ae7bdd42114cea8df9b62d13cdc6c4
Jagge1 <hellgrenjacob@hotmail.com>
2025-05-21 22:59:04 +0200

    Added some styling to the navbar

--- Portfolio/src/components/HeroSection.jsx
+++ Portfolio/src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ export default function HeroSection(){
       <img src="../assets/Portfolio-Portrait-2.png" alt="Illustration portrait" className="portrait"/>
 
       <article className='description-container'>
-        <h4 className='title'>Hi, my name is Jacob Hellgren</h4>
+        <h4 className='title'>Hi, my name is Jacob Hellgren...</h4>
 
       </article>
       
