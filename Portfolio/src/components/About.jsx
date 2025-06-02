156b42bcec6ca4c01c5d6df4a6e2c277fb9526a8
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-02 23:04:06 +0200

    Started on the about section, added icons for the thech stack

--- Portfolio/src/components/About.jsx
+++ Portfolio/src/components/About.jsx
@@ -2,9 +2,25 @@ import './styles/about.css'
 
 export default function About(){
   return (
-    <div className='about-container' id='about'>
-      <h1>About me</h1>
-    </div>
-    
+    <section className='about-container' id='about'>
+      <h1 className='about-me-title'>About me</h1>
+      <h3 className='stack-title'>This is my Tech Stack</h3>
+
+      <article className='stack-container'>
+        <img src="../assets/js-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/css-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/html-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/firebase-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/git-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/java-logo.png" className='tech-icon' alt="" />   
+        <img src="../assets/mongodb-compass-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/mysql-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/node-js-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/python-logo.png" className='tech-icon' alt="" />
+        <img src="../assets/react-logo.png" className='tech-icon' alt="" />
+
+      </article>
+
+    </section>
   )
 }
