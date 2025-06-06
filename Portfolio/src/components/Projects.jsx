726f4d5dd20dab5b78b8f57fb0928bb3e20995e9
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-06 16:01:01 +0200

    Basic but functioning project section completed

--- Portfolio/src/components/Projects.jsx
+++ Portfolio/src/components/Projects.jsx
@@ -8,17 +8,40 @@ export default function Projects(){
       <section className='projects'>
 
       <article className='project-card'>
-        <h4 className='project-card-title'>E-commerce website</h4>
+        <img src="../assets/house18-logo.png" alt="house18-logo" className='project-img' id='house18-icon'/>
+        <h4 className='project-card-title'>HOUSE 18</h4>
+
+        <p className='project-description'>
+          House 18 is a fullstack web application built as part of a university
+          project during the course Full Stack Development at Kristianstad University.
+          The app simulates a modern clothing store with user authentication, a responsive shopping
+          exprerience and admin product management.
+        </p>
+
+        <a href="https://github.com/nickem00/house-18" className='project-link' target='_blank'>View project</a>
+        <a href="https://house-18.vercel.app/" className='project-link' target='_blank'>View website</a>
+
       </article>
 
       <article className='project-card'>
-        <h4 className='project-card-title'>Mental Health application</h4>
+        <img src="../assets/solace-logo.png" alt="solace-logo" className='project-img' id='solace-icon'/>
+        <h4 className='project-card-title'>SOLACE</h4>
+        
+        <p className='project-description' id='top-paragraph'>
+          This desktop application is designed to help people deal with stress and anxiety by
+          allowing them to track their mood and stress levels during a period of time.
+        </p>
+        <br />
+        <p className='project-description' id='bottom-paragraph'>
+          The application was created during the course Agile Development and was meant as an
+          exercise to learn agile development and Scrum
+        </p>
+        
+        <a href="https://github.com/HugNil/Solace" className='project-link' target='_blank'>View project</a>
+
       </article>
 
       </section>
-
-
-
     </section>
   )
 }
