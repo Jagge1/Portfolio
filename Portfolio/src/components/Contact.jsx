1bd5f1b47ad307d3c2816e5236364216f23a53dd
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-06 13:47:23 +0200

    Basic functional contact section

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -3,8 +3,29 @@ import './styles/contact.css'
 export default function Contact(){
   return(
     <section className="contact-container" id="contact">
+
       	<h1 className='contact-title'>Contact</h1>
         <h4 className='secondary-title'>Where to find me</h4>
+
+        <section className='card-container'>
+
+        <article className='contact-card'>
+          <img src="../assets/github-icon.png" alt="github-icon" className='contact-icon'/>
+          <a href="https://github.com/Jagge1" target='_blank' className='icon-link'>My Github</a>
+        </article>
+
+        <article className='contact-card'>
+          <img src="../assets/mail-icon.png" alt="mail-icon" className='contact-icon'/>
+          <a href="mailto:hellgrenjacob@hotmail.com" className='icon-link'>My Email</a>
+        </article>
+
+        <article className='contact-card'>
+          <img src="../assets/linkedin-icon.png" alt="linkedin-icon" className='contact-icon'/>
+          <a href="https://se.linkedin.com/in/jacob-hellgren-a88967325?trk=people-guest_people_search-card"className='icon-link' target='_blank'>My LinkedIn</a>
+        </article>
+
+        </section>
+
     </section>
   )
 }
