25f1c196ed59d2a51d45e6c347f2d14c79814ee1
Jagge1 <hellgrenjacob@hotmail.com>
2025-06-30 22:34:30 +0200

    Minor styling changes

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -18,17 +18,17 @@ export default function Contact(){
 
         <article className='contact-card'>
           <img src="../assets/github-icon.png" alt="github-icon" className='contact-icon'/>
-          <a href="https://github.com/Jagge1" className='icon-link' target='_blank'>My Github <span className='arrow-link'><FontAwesomeIcon icon={faArrowRight} /></span></a>
+          <a href="https://github.com/Jagge1" className='icon-link' target='_blank'>Github <span className='arrow-link'><FontAwesomeIcon icon={faArrowRight} /></span></a>
         </article>
 
         <article className='contact-card'>
           <img src="../assets/mail-icon.png" alt="mail-icon" className='contact-icon'/>
-          <a href="mailto:hellgrenjacob@hotmail.com" className='icon-link' target='_blank'>My Email <span className='arrow-link'><FontAwesomeIcon icon={faArrowRight} /></span></a>
+          <a href="mailto:hellgrenjacob@hotmail.com" className='icon-link' target='_blank'>Email <span className='arrow-link'><FontAwesomeIcon icon={faArrowRight} /></span></a>
         </article>
 
         <article className='contact-card'>
           <img src="../assets/linkedin-icon.png" alt="linkedin-icon" className='contact-icon'/>
-          <a href="https://se.linkedin.com/in/jacob-hellgren-a88967325?trk=people-guest_people_search-card" className='icon-link' target='_blank'>My LinkedIn <span className='arrow-link'><FontAwesomeIcon icon={faArrowRight} /></span></a>
+          <a href="https://se.linkedin.com/in/jacob-hellgren-a88967325?trk=people-guest_people_search-card" className='icon-link' target='_blank'>LinkedIn <span className='arrow-link'><FontAwesomeIcon icon={faArrowRight} /></span></a>
         </article>
 
         </section>
